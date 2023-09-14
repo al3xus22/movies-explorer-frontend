939f281382ec19ff6accea02caccdb033f28958d
Aleksey Oleynik <al3xus22@yandex.ru>
2023-09-15 02:35:20 +0300

    feat: кнопка еще, сохраненные фильмы и устранение багов

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
         <div className="footer__columns">
           <p className="footer__copyright footer__text">© 2023</p>
           <nav className="footer__links">
-            <a className="footer__link footer__text hover-effect link-effect" target="_blank" href="https://practicum.yandex.ru/profile/web/">Яндекс.Практикум</a>
-            <a className="footer__link footer__text hover-effect link-effect" target="_blank" href="https://github.com/al3xus22">Github</a>
+            <a className="footer__link footer__text hover-effect link-effect" target="_blank" href="https://practicum.yandex.ru/profile/web/" rel="noreferrer">Яндекс.Практикум</a>
+            <a className="footer__link footer__text hover-effect link-effect" target="_blank" href="https://github.com/al3xus22" rel="noreferrer">Github</a>
           </nav>
         </div>
       </div>
--- src/components/main/portfolio/portfolio.jsx
+++ src/components/main/portfolio/portfolio.jsx
@@ -8,20 +8,20 @@ function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
-          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect">Статичный сайт</a>
-          <a href="https://al3xus22.github.io/how-to-learn/"  target="_blank" className="portfolio__link hover-effect link-effect">
+          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect" rel="noreferrer">Статичный сайт</a>
+          <a href="https://al3xus22.github.io/how-to-learn/"  target="_blank" className="portfolio__link hover-effect link-effect" rel="noreferrer">
             <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
           </a>
         </li>
         <li className="portfolio__list-item">
-          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect">Адаптивный сайт</a>
-          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__link hover-effect link-effect">
+          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect" rel="noreferrer">Адаптивный сайт</a>
+          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__link hover-effect link-effect" rel="noreferrer">
             <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
             </a>
         </li>
         <li className="portfolio__list-item">
-          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect">Одностраничное приложение</a>
-          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__link hover-effect link-effect">
+          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__item-text hover-effect link-effect" rel="noreferrer">Одностраничное приложение</a>
+          <a href="https://al3xus22.github.io/how-to-learn/" target="_blank" className="portfolio__link hover-effect link-effect" rel="noreferrer">
             <img src={portfolioLink} alt="Ссылка" className="portfolio__link-image"/>
           </a>
         </li>
