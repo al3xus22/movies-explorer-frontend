bd01d7f3e8fb7d48c424c07c43a04b6d3895a30b
Aleksey Oleynik <al3xus22@yandex.ru>
2023-09-16 11:12:59 +0300

    feat: кнопка еще, сохраненные фильмы и устранение багов

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -27,6 +27,7 @@ function Movies({
     const saveState = localStorage.getItem("isShortFilm");
     return saveState ? JSON.parse(saveState) : [];
   });
+  const [showedShortMovies, setShowedShortMovies] = useState([])
   const [displayedMovies, setDisplayedMovies] = useState([]);                //Отображамые фильмы на странице
   const [cardsRow, setCardsRow] = useState(0);                               //Карточек в ряду
   const [showCards, setShowCards] = useState(0);                             //Фильмов на странице изначально
@@ -58,7 +59,7 @@ function Movies({
   }
 
   //Отображаемые на странице карточки и данные для кнопки Ещё--------------------------------------------------------
-  const isAllMoviesDisplayed = searchMovies.length === displayedMovies.length || movies.length === displayedMovies.length;
+  const isAllMoviesDisplayed = searchMovies.length === displayedMovies.length || movies.length === displayedMovies.length || showedShortMovies.length === displayedMovies.length;
 
   //Изменение ширины
   const handleResize = () => {
@@ -105,6 +106,7 @@ function Movies({
     if (isShortFilm && searchMovies.length > 0) {
       // Фильтр короткометражек
       const filteredShortFilms = filterShortFilms(searchMovies);
+      setShowedShortMovies(filteredShortFilms);
       setDisplayedMovies(filteredShortFilms.slice(0, showCards));
     } else {
       if (query && searchMovies.length > 0) {
